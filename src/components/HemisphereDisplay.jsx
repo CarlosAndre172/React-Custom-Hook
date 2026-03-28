4a39c88d9e99d2965d9f08b8e03ede86a3d9d51c
ramonbezerra <ramon.bezerra@outlook.com>
2026-03-28 02:06:37 -0300

    first commit

---
+++ src/components/HemisphereDisplay.jsx
@@ -0,0 +1,19 @@
+export const HemisphereDisplay = () => {
+
+  const currentLocation = null;
+
+  if (!currentLocation) {
+    return <p>{'Sorry, location is currently unavailable'}</p>;
+  }
+
+  // currentLocation should have a .latitude property
+  if (currentLocation.latitude === 0) {
+    return <p>You are at the equator!</p>;
+  }
+
+  const hemisphere = currentLocation.latitude > 0
+    ? 'northern'
+    : 'southern';
+
+  return <p>You are in the {hemisphere} hemisphere.</p>;
+}
---
+++ src/components/LongitudeLatitudeDisplay.jsx
@@ -0,0 +1,19 @@
+export const LongitudeLatitudeDisplay = () => {
+  const currentLocation = null;
+
+  if (!currentLocation) {
+    return <p>{'Sorry, location is currently unavailable'}</p>;
+  }
+
+  const { latitude, longitude } = currentLocation;
+
+  return (
+    <section>
+      <p>{latitude}° {latitude > 0 ? 'N' : 'S'}</p>
+      <hr />
+      <p>{longitude}° {longitude > 0 ? 'E' : 'W'}</p>
+    </section>
+  );
+}
+
+
